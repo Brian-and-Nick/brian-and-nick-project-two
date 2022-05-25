dca23a3194da948a55ade9c9b7ee271ed6b4b37b
Brian Yoon <yoon.brian91@gmail.com>
2022-05-24 23:45:22 -0400

    JS fixed to work in Chrome and in Firefox

--- script/script.js
+++ script/script.js
@@ -6,6 +6,9 @@ musicApp.method = `chart.gettopartists`;
 musicApp.format = `json`
 musicApp.limit = ""; 
 
+musicApp.select = document.querySelector('select');
+musicApp.artistInfoSection = document.querySelector('.artistInfo');
+
 musicApp.init = () => {
     musicApp.getArtist();
 }
@@ -34,7 +37,6 @@ musicApp.getArtist = function(){
 }
 
 
-
 // Grab the names of the artists and populate it in the dropdown menu.
 // The function needs to create 'option' and put the names of the artists in the drop down menu.
 // Add a value attribute and make it equal to the artist's name. 
@@ -44,28 +46,32 @@ musicApp.getArtist = function(){
 
 musicApp.dropdownOptions = (objectOfArtists) => {
 
-    const select = document.querySelector('select');
     const artistArray = objectOfArtists.artists.artist;
 
     artistArray.forEach((individualArtist) => {
 
         const optionEl = document.createElement('option');
         optionEl.innerText = `${individualArtist.name}`;
-        select.appendChild(optionEl);
+        optionEl.value = `${individualArtist.name}`;
+        musicApp.select.appendChild(optionEl);
 
-            optionEl.addEventListener('click', () => {
+        musicApp.select.addEventListener('change', function(){
 
-                const artistInfoSection = document.querySelector('.artistInfo');
+            if (this.value === optionEl.value){
 
-                artistInfoSection.innerHTML = 
-                `<h3>${individualArtist.name}</h3>
-                <h4>Play Count: ${individualArtist.playcount}</h4>
-                <p><a href="${individualArtist.url}" target="_blank">Last.fm Profile</a>
-                </p>`;
-            });
+            const artistInfoSection = document.querySelector('.artistInfo');
+            artistInfoSection.innerHTML = `<h3>${individualArtist.name}</h3>
+            <h4>Play Count: ${individualArtist.playcount}</h4>
+            <p><a href="${individualArtist.url}" target="_blank">Last.fm Profile</a>
+            </p>`;
 
+            } 
+
+        })
     });
+
 };
+
 
 musicApp.randomArtist = (objectOfArtists) => {
 
@@ -89,8 +95,4 @@ musicApp.randomArtist = (objectOfArtists) => {
 
 
 
-
-
-
 musicApp.init();
-
